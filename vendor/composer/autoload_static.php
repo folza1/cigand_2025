7f5f58bdfd288ff688b1f872f3fc7655774682ed
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-02-18 04:04:31 +0100

    Deploy 26.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -355,8 +355,8 @@ class ComposerStaticInit6977c4291e2c53e988043ff180e778ba
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
